a813d345dbb10575178948b53533ee24c914799f
GabrielRossatto <rossattogabriel@gmail.com>
2023-10-04 15:31:53 -0300

    Interface da playstation store com angular 5

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent implements OnInit {
+
+  @Input()
+  gamePrice: string = ""
+  @Input()
+  gameConsole: string = "R$"
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
